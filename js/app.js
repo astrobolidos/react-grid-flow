cd014765ca1ee54eb7cd3f30501292dfcc868084
astrobolidos <lisandroalavez@gmail.com>
2015-04-05 22:46:08 +0100

    get grid ground work

--- js/app.js
+++ js/app.js
@@ -5,7 +5,13 @@ var React = require('react');
 
 var App = React.createClass({
 	render: function() {
-		return <p>welcome to react grid flow</p>
+		return (
+			<div class="grid">
+				<div class="col-1-8">
+					welcome to react grid flow
+				</div>
+			</div>
+		)
 	}
 });
 
