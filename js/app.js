219e4e90d4167fdf58373d6ea61fdf29daafc669
astrobolidos <lisandroalavez@gmail.com>
2015-04-06 09:49:27 +0100

    added toolbar control

--- js/app.js
+++ js/app.js
@@ -3,11 +3,29 @@ require('../styles/site.scss');
 
 var React = require('react');
 
+var Toolbar = React.createClass({
+	render: function() {
+		return (
+			<div className="grid">
+				<div className="col-1-8">&nbsp;</div>
+				<div className="col-5-6" styles={{textAlign:'center'}}>
+					{this.props.children}
+				</div>
+				<div className="col-1-8">&nbsp;</div>
+			</div>
+		)
+	}
+});
+
 var App = React.createClass({
 	render: function() {
 		return (
-			<div class="grid">
-				<div class="col-1-8">
+			<div className="grid">
+				<Toolbar>
+					<p>This is my toolbar</p>
+				</Toolbar>
+
+				<div className="col-1-8">
 					welcome to react grid flow
 				</div>
 			</div>
