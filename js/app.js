a945511db1ffe760537b7b69f0e9d130918bd795
astrobolidos <lisandroalavez@gmail.com>
2015-06-18 11:11:20 +0100

    added animation on enter

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-window.React = require('react');
-require('../styles/site.scss');
+window.React = require('react/addons');
+require('../styles/site.scss'); 
 
 var React = require('react');  
 var Toolbar = require('./components/grid/toolbar');
+var CSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 var SideBar = React.createClass({
 	render: function() {
@@ -14,10 +15,46 @@ var SideBar = React.createClass({
 	}
 });
 
+var Body = React.createClass({
+	render: function() {
+		return <div className="body col-4-5">
+			{this.props.children}
+		</div>
+	}
+})
+
+var Control = React.createClass({
+	render: function() {
+		return <input type="text" name="iText" className="clearBoth" value={this.props.value} />
+	}
+})
+
+
+var Dynamic = React.createClass({
+	getInitialState: function() {
+		return { info: [] }
+	},
+	handleAddClick: function(event) {
+		this.state.info.push({name: 'control' + this.state.info.length, value:this.state.info.length});
+		this.setState({info: this.state.info})
+	},
+	render: function() {
+		return <div className="col-2-3">
+			<input type="button" name="btnAdd" onClick={this.handleAddClick} value="Add control" />
+			<br/>
+			<CSSTransitionGroup transitionName="update" transitionAppear={true}>
+				{this.state.info.map(function(m, i){
+					return <Control key={i} value={m.value} />
+				})}
+			</CSSTransitionGroup>
+		</div>
+	}
+})
+
 var App = React.createClass({
 	render: function() {
 		return (
-			<div className="grid">			
+			<div className="body grid">			
 				<Toolbar>
 					<p>This is my toolbar with centralized bar</p>
 				</Toolbar>
@@ -28,7 +65,10 @@ var App = React.createClass({
 						<li>People</li>												
 					</ul>
 				</SideBar>
-
+				<Body>
+					<h1>Welcome</h1>
+					<Dynamic />
+				</Body>
 			</div>
 		)
 	}
