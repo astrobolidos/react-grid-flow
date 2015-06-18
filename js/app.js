82d8f46c03a945c87f28ae17e069525008eca6be
astrobolidos <lisandroalavez@gmail.com>
2015-06-18 12:06:54 +0100

    added update animations

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 window.React = require('react/addons');
 require('../styles/site.scss'); 
+require('../styles/animation.scss');
 
 var React = require('react');  
 var Toolbar = require('./components/grid/toolbar');
@@ -25,7 +26,8 @@ var Body = React.createClass({
 
 var Control = React.createClass({
 	render: function() {
-		return <input type="text" name="iText" className="clearBoth" value={this.props.value} />
+		var classNames = 'clearBoth ' + this.props.className;
+		return <input type="text" name="iText" className={classNames} value={this.props.value} />
 	}
 })
 
@@ -35,16 +37,33 @@ var Dynamic = React.createClass({
 		return { info: [] }
 	},
 	handleAddClick: function(event) {
-		this.state.info.push({name: 'control' + this.state.info.length, value:this.state.info.length});
+		this.state.info.push({name: 'control' + this.state.info.length, value:this.state.info.length, className:''});
 		this.setState({info: this.state.info})
+	},
+	handleUpdateClick: function(event) {
+		for(i = 0; i < this.state.info.length; i++) {
+			this.state.info[i].value = this.state.info[i].value + 3;
+			this.state.info[i].className = 'pulse';
+		}
+		this.setState({info: this.state.info});
+	},
+	handleCleanClick: function(event) {
+		for(i = 0; i < this.state.info.length; i++) {
+			this.state.info[i].className = '';
+		}
+		this.setState({info: this.state.info});		
 	},
 	render: function() {
 		return <div className="col-2-3">
 			<input type="button" name="btnAdd" onClick={this.handleAddClick} value="Add control" />
+			&nbsp;
+			<input type="button" name="btnUpdate" onClick={this.handleUpdateClick} value="Update values" />
+			&nbsp;
+			<input type="button" name="btnClean" onClick={this.handleCleanClick} value="Clean styles" />
 			<br/>
 			<CSSTransitionGroup transitionName="update" transitionAppear={true}>
 				{this.state.info.map(function(m, i){
-					return <Control key={i} value={m.value} />
+					return <Control key={i} value={m.value} className={m.className} />
 				})}
 			</CSSTransitionGroup>
 		</div>
